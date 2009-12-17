b361c47929dfc8c8a7a704fa4ca2f78c9a771330
indezinez <indezinez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-17 16:34:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/the-daily-hadith-widget/tags/1.0@183878 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== The Daily Hadith Widget ===
+Contributors: Umar Sheikh
+Tags: plugin, islam, quran, koran, muslim, hadith, widget
+Donate link: http://www.islamicnature.com/donate.php
+Requires at least: 2.8
+Tested up to: 2.8.x
+Stable tag: 1.0
+
+A widget that displays a different hadith a day
+
+== Description ==
+
+A widget that displays a different hadith a day. It takes hadiths from 
+Sahih Bukhari, Sahih Muslim and more. It also gives a link at the end 
+of the hadith to iknowledge.islamicnature.com where you can read more hadiths.
+Tags: koran, quran, muslim, islam, plugin, hadith, widget.
+
+== Installation ==
+
+1. Extract the folder into the wp-content/plugins directory.
+2. Go to the plugins section in the admin backend and activate the widget.
+3. Go to Appearance > Widgets and add the widget to your sidebar.
+4. Enjoy.
+
+== Screenshots ==
+
+N/A
+
+== Frequently Asked Questions ==
+
+N/A
---
+++ thedailyhadith.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Plugin Name: The Daily Hadith Widget
+Plugin URI: http://iknowledge.islamicnature.com/extras.php
+Description: A widget that displays a different hadith a day
+Author: Umar Sheikh
+Author URI: http://www.indezinez.com
+Version: 1.0
+*/
+
+add_action('widgets_init', 'load_daily_hadith');
+
+function load_daily_hadith(){
+  if(function_exists('register_widget')){
+    register_widget('Daily_Hadith');
+	}
+}
+
+class Daily_Hadith extends WP_Widget {
+
+	function Daily_Hadith(){
+	
+		$widget_ops = array('classname' => 'dailyhadith', 'description' => __('A widget that displays a different hadith a day', 'dailyhadith'));
+		$control_ops = array('width' => 300, 'height' => 350, 'id_base' => 'daily-hadith');
+		$this->WP_Widget('daily-hadith', __('Daily Hadith', 'dailyhadith'), $widget_ops, $control_ops);
+	
+	}
+
+	function widget($args, $instance){
+	
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title']);
+		echo $before_widget;
+		if($title){
+			echo $before_title.$title.$after_title;
+		}
+		echo '
+		<p>
+		<script type="text/javascript" src="http://iknowledge.islamicnature.com/dh_script.php"></script>
+		</p>
+		';
+		echo $after_widget;
+		
+	}
+
+	function update($new_instance, $old_instance){
+	
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
+		
+	}
+
+	function form($instance){
+
+		$defaults = array('title' => __('Daily Hadith', 'dailyhadith'));
+		$instance = wp_parse_args((array) $instance, $defaults); ?>
+
+		<!-- Widget Title: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'hybrid'); ?></label>
+			<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+		</p>
+
+	<?php
+	}
+}
+
+?>
